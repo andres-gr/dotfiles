7acb8e3731396e77500d3c505de6c4d01d9e2467
Andres Garcia <andres.gar@gmail.com>
2022-11-01 18:43:30 -0600

    updated notify

--- nvim_agr/lua/agr/configs/notify.lua
+++ nvim_agr/lua/agr/configs/notify.lua
@@ -3,7 +3,8 @@ if not notify_status_ok then return end
 
 notify.setup {
   background_colour = '#000000',
-  stages = 'fade',
+  stages = 'fade_in_slide_out',
+  timeout = 1500,
 }
 
 vim.notify = notify
