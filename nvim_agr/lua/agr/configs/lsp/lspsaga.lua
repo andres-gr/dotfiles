29e64ed2064f4c7a43dc943076526c64ded0446a
Andres Garcia <andres.gar@gmail.com>
2022-12-07 16:03:17 -0600

    updated lspsaga config

--- nvim_agr/lua/agr/configs/lsp/lspsaga.lua
+++ nvim_agr/lua/agr/configs/lsp/lspsaga.lua
@@ -2,6 +2,13 @@ local saga_status, saga = pcall(require, 'lspsaga')
 if not saga_status then return end
 
 saga.init_lsp_saga {
+  code_action_lightbulb = {
+    cache_code_action = false,
+    enable = true,
+    enable_in_insert = false,
+    update_time = 250,
+    virtual_text = false,
+  },
   definition_action_keys = {
     edit = '<CR>', -- Use enter to open file in preview definition
   },
