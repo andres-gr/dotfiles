abf3b90bf260ad9a40557d6368b409f46012175d
Andres Garcia <andres.gar@gmail.com>
2023-04-11 10:42:51 -0600

    updated mason null ls setup

--- nvim_agr/lua/agr/lazy/plugins/lsp/mason_null_ls.lua
+++ nvim_agr/lua/agr/lazy/plugins/lsp/mason_null_ls.lua
@@ -3,13 +3,14 @@ local M = {}
 M.setup = function ()
   local mason_nls = require 'mason-null-ls'
 
-  mason_nls.setup {}
-
-  mason_nls.setup_handlers {
-    function (server)
-      require 'agr.core.utils'.null_ls_register(server)
-    end,
+  mason_nls.setup {
+    handlers = {
+      function (server)
+        require 'agr.core.utils'.null_ls_register(server)
+      end,
+    },
   }
+
 end
 
 return M
