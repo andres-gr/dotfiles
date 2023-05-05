3f276ebe427e3b989cbc8c93aaecb33b76959b13
Andres Garcia <andres.gar@gmail.com>
2023-05-05 14:45:44 -0600

    fix for telescope buf enter in insert mode

--- nvim_agr/lua/agr/core/autocmds.lua
+++ nvim_agr/lua/agr/core/autocmds.lua
@@ -161,3 +161,10 @@ cmd({ 'VimEnter', 'ColorScheme' }, {
   group = agr,
 })
 
+cmd('WinLeave', {
+	callback = function ()
+		if vim.bo.ft == 'TelescopePrompt' and vim.fn.mode() == 'i' then
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'i', false)
+		end
+	end,
+})
