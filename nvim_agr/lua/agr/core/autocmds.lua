26923a720556ef2199b796553a9fead720e0dfc4
Andres Garcia <andres.gar@gmail.com>
2023-08-30 13:03:26 -0600

    added quitpre autocmd

--- nvim_agr/lua/agr/core/autocmds.lua
+++ nvim_agr/lua/agr/core/autocmds.lua
@@ -179,6 +179,30 @@ cmd('BufReadPost', {
   group = general,
 })
 
+
+cmd('QuitPre', {
+  callback = function()
+    local tree_wins = {}
+    local floating_wins = {}
+    local wins = vim.api.nvim_list_wins()
+    for _, w in ipairs(wins) do
+      local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+      if bufname:match('neo--tree') ~= nil then
+        table.insert(tree_wins, w)
+      end
+      if vim.api.nvim_win_get_config(w).relative ~= '' then
+        table.insert(floating_wins, w)
+      end
+    end
+    if 1 == #wins - #floating_wins - #tree_wins then
+      -- Should quit, so we close all invalid windows.
+      for _, w in ipairs(tree_wins) do
+        vim.api.nvim_win_close(w, true)
+      end
+    end
+  end
+})
+
 local agr = augroup('agr_highlights', { clear = true })
 cmd({ 'VimEnter', 'ColorScheme' }, {
   callback = function ()
