9023c668b5850d2ec1813fd0d70affd652142c56
Andres Garcia <andres.gar@gmail.com>
2023-12-04 11:17:45 -0600

    updated luasnip config

--- nvim_agr/lua/agr/lazy/plugins/cmp.lua
+++ nvim_agr/lua/agr/lazy/plugins/cmp.lua
@@ -6,7 +6,10 @@ local M = {
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-nvim-lua',
     'hrsh7th/cmp-path',
-    'L3MON4D3/LuaSnip',
+    {
+      'L3MON4D3/LuaSnip',
+      run = 'make install_jsregexp',
+    },
     'rafamadriz/friendly-snippets',
     'saadparwaiz1/cmp_luasnip',
   },
