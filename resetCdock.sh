3e797398e96353d72786d245f7ac6d86aaf26813
Andres Garcia <andres.gar@gmail.com>
2024-02-12 11:02:57 -0600

    updated reset cdock script

--- resetCdock.sh
+++ resetCdock.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Automator script
+#
+# #!/bin/bash
+#
+# export PATH="$PATH:/usr/local/bin/"
+# export DOTFILES="$HOME/devel/dotfiles"
+#
+# resetCdock.sh && open -a cDock
+
 $(rm -rf "$HOME/Library/Application Support/cDock")
 
 $(mkdir "$HOME/Library/Application Support/cDock")
