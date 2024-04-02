29b0fa325bb97435926d6950ebf4784524f0796f
Andres Garcia <andres.gar@gmail.com>
2024-04-02 14:04:09 -0600

    updated ai config

--- nvim_agr/lua/agr/lazy/plugins/ai.lua
+++ nvim_agr/lua/agr/lazy/plugins/ai.lua
@@ -5,9 +5,7 @@ local A = {
   event = 'InsertEnter',
 }
 
-A.config = function()
-  -- vim.g.codeium_tab_fallback = '<Tab>'
-
+A.config = function ()
   local keymap = require 'agr.core.utils'.keymap
   local map = keymap.map
   local desc_opts = function (desc)
@@ -19,16 +17,19 @@ A.config = function()
   end
 
   map('i', '<Tab>', function () return vim.fn['codeium#Accept']() end, desc_opts('Codeium accept suggestion'))
+  map('n', '<M-g>', function () return vim.fn['codeium#Chat']() end, desc_opts('Codeium chat'))
 
   local utils = require 'agr.core.utils'
   local cmp = utils.has_plugin 'cmp'
 
   if cmp then
-    cmp.event:on('menu_opened', function()
+    cmp.event:on('menu_opened', function ()
+      vim.b.codeium_render = false
       vim.cmd [[ Codeium DisableBuffer ]]
     end)
 
-    cmp.event:on('menu_closed', function()
+    cmp.event:on('menu_closed', function ()
+      vim.b.codeium_render = true
       vim.cmd [[ Codeium EnableBuffer ]]
     end)
   end
