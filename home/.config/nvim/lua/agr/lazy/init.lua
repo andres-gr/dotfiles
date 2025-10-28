ba6b9e1a5c949b055b9389515780e3bac79accc2
Andres Garcia <andres.gar@gmail.com>
2025-10-28 08:08:43 -0600

    update lazy bootstrap

--- home/.config/nvim/lua/agr/lazy/init.lua
+++ home/.config/nvim/lua/agr/lazy/init.lua
@@ -1,39 +1,59 @@
 local fn = vim.fn
 
--- Automatically install packer
-local lazypath = fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
+-- Bootstrap lazy.nvim
+local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable", -- latest stable release
+		lazyrepo,
+		lazypath,
+	})
+
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+
+		fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 local lazy_opts = {
-  defaults = {
-    lazy = true,
-    version = '*',
-  },
-  install = { colorscheme = { 'dracula' } },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        'gzip',
-        'matchit',
-        'matchparen',
-        'netrwPlugin',
-        'tarPlugin',
-        'tohtml',
-        'tutor',
-        'zipPlugin',
-      },
-    },
-  },
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	defaults = {
+		lazy = false,
+		version = "*",
+	},
+	install = { colorscheme = { "dracula" } },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+	spec = {
+		-- { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
+		{ import = "agr.lazy.plugins" },
+	},
 }
 
-require 'lazy'.setup('agr.lazy.plugins', lazy_opts)
+require("lazy").setup(lazy_opts)
