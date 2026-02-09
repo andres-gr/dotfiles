2172240215c41abf9defbacb209d3ed7cf4f72c0
Andres Garcia <andres.gar@gmail.com>
2026-02-09 15:22:05 -0600

    updated luasnip version conf

--- home/.config/nvim/lua/agr/lazy/plugins/cmp.lua
+++ home/.config/nvim/lua/agr/lazy/plugins/cmp.lua
@@ -10,6 +10,7 @@ local M = {
     {
       'L3MON4D3/LuaSnip',
       run = 'make install_jsregexp',
+      version = 'v2.*',
     },
     'rafamadriz/friendly-snippets',
     'saadparwaiz1/cmp_luasnip',
