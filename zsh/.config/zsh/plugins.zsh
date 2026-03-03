85ce04f246bf41bf7a1437fe964dc2be399c3f4b
Andres Garcia <andres.gar@gmail.com>
2026-03-02 19:38:04 -0600

    updated zsh highlights plugin, back to fast highlights

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -36,44 +36,14 @@ zinit load hlissner/zsh-autopair
 # Syntax Highlighting (must load last)
 # ----------------------------------------
 zinit ice wait"1" silent
-zinit load zsh-users/zsh-syntax-highlighting
+zinit load zdharma-continuum/fast-syntax-highlighting
 
 # ----------------------------------------
 # Custom Highlight Styles
 # (applied after plugin loads)
 # ----------------------------------------
 
-# After zsh-syntax-highlighting is loaded
-typeset -gA ZSH_HIGHLIGHT_STYLES
-
-# Aliases
-ZSH_HIGHLIGHT_STYLES[alias]='fg=green'
-ZSH_HIGHLIGHT_STYLES[global-alias]='fg=green'
-ZSH_HIGHLIGHT_STYLES[suffix-alias]='fg=green'
-
-# Builtins / commands
-ZSH_HIGHLIGHT_STYLES[builtin]='fg=green'
-ZSH_HIGHLIGHT_STYLES[command]='fg=cyan,bold'
-ZSH_HIGHLIGHT_STYLES[function]='fg=magenta'
-
-# Paths
-ZSH_HIGHLIGHT_STYLES[autodirectory]='fg=blue'
-ZSH_HIGHLIGHT_STYLES[path]='fg=blue'
-ZSH_HIGHLIGHT_STYLES[path_prefix]='fg=blue'
-
-# Flags
-ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=yellow'
-ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=yellow'
-
-# Fallback command word
-ZSH_HIGHLIGHT_STYLES[arg0]='fg=white'
-
-# Strings
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=green'
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=green'
-
-# Comments
-ZSH_HIGHLIGHT_STYLES[comment]='fg=8'
-
-# Errors
-ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=red,bold'
+typeset -gA FAST_HIGHLIGHT_STYLES
+FAST_HIGHLIGHT_STYLES[path-to-dir]="fg=cyan"
+FAST_HIGHLIGHT_STYLES[precommand]="fg=green"
+FAST_HIGHLIGHT_STYLES[suffix-alias]="fg=green"
