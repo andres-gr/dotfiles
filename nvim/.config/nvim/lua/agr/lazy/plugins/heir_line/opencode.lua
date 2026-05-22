73dc3e40401f324b8209bcb7fdd6c6189d6d3379
Andres Garcia <andres.gar@gmail.com>
2026-05-22 11:52:12 -0600

    nvim - updated opencode binds, added statusline widget

---
+++ nvim/.config/nvim/lua/agr/lazy/plugins/heir_line/opencode.lua
@@ -0,0 +1,16 @@
+local O = {}
+
+O.setup = function ()
+  local opencode_ok, opencode = pcall(require, 'opencode')
+
+  return {
+    condition = function ()
+      return opencode_ok
+    end,
+    provider = function ()
+      return opencode.statusline()
+    end,
+  }
+end
+
+return O
