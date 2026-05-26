13d0d2cf03484ccefc759f9f371f3233050e68b7
Andres Garcia <andres.gar@gmail.com>
2026-05-25 22:49:34 -0600

    nvim - fixed/updated opencode confs

--- nvim/.config/nvim/lua/agr/lazy/plugins/snacks.lua
+++ nvim/.config/nvim/lua/agr/lazy/plugins/snacks.lua
@@ -14,12 +14,12 @@ S.opts = {
   },
   picker = { -- Enhances `select()`
     actions = {
-      opencode_send = function(...) return require("opencode").snacks_picker_send(...) end,
+      opencode_send = function(...) return require('opencode').snacks_picker_send(...) end,
     },
     win = {
       input = {
         keys = {
-          ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+          ['<a-a>'] = { 'opencode_send', mode = { 'n', 'i' } },
         },
       },
     },
