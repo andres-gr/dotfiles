09e152712a405a3ec9c7b0a75ea7d542768af9fd
Andres Garcia <andres.gar@gmail.com>
2026-06-08 17:20:50 -0600

    arch hypr - minor hymission conf fix

--- arch-hyprland/.config/hypr/plugins/hymission.lua
+++ arch-hyprland/.config/hypr/plugins/hymission.lua
@@ -13,6 +13,7 @@ if hl.plugin.hymission ~= nil then
       hymission = {
         max_preview_scale = 0.75,
         show_focus_indicator = false,
+        toggle_switch_mode = true,
         workspace_strip_anchor = 'left',
         workspace_strip_gap = 32,
         workspace_strip_thickness = 240,
