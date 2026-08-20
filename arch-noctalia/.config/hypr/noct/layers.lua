b4ee388247d810fd1150469d324b0fc267c217c3
Andres Garcia <andres.gar@gmail.com>
2026-08-20 12:37:01 -0600

    arch noctalia - v5 init changes

--- arch-noctalia/.config/hypr/noct/layers.lua
+++ arch-noctalia/.config/hypr/noct/layers.lua
@@ -1,9 +1,10 @@
 hl.layer_rule {
   name = 'noctalia',
   match = {
-    namespace = 'noctalia-background-.*$',
+    namespace = '^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd|window-switcher)$',
   },
   blur = true,
   blur_popups = true,
   ignore_alpha = 0.5,
+  no_anim = true,
 }
